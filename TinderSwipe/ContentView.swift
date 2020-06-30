8222cd5c9fb1598173614e8f92535f42ac9b1a34
Aakash <aakash.bscs@gmail.com>
2020-06-30 20:55:10 +0500

    Initial Commit

---
+++ TinderSwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinderSwipe
+//
+//  Created by Aakash on 30/06/2020.
+//  Copyright © 2020 Minhasoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
